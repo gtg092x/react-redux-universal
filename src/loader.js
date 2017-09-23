5c36e6188ada0268677f6ae907a1cf66a4052f66
gtg092x <mdrake@mediadrake.com>
2017-09-23 13:05:16 -0700

    move to async

--- src/loader.js
+++ src/loader.js
@@ -9,53 +9,58 @@ const getRequests = universal => getUniversalCaches(universal).filter(hash => !!
 
 const taut = () => true;
 
-export default function appServer(renderer, store, timeout = 30000, next = taut) {
-  return new Promise((resolve, reject) => {
-    let html;
-    let to;
-    let unsub;
-    let dispatched = false;
-    const checkSub = () => {
-      const finalState = store.getState();
-      const requests = getRequests(selectUniversal(finalState));
-      if (requests.length === 0 || !next(finalState)) {
-        try {
-          const nextHtml = dispatched ? renderer({ store }) : html;
-          if (dispatched) {
-            // if you dispatched during render will do one final render after requests have resolved
-            // giving you a chance to mount child containers
-            dispatched = false;
-            html = nextHtml;
-            setImmediate(checkSub);
-            return;
-          }
-          clearTimeout(to);
-          unsub();
-          resolve({ html: nextHtml, state: finalState });
-        } catch (e) {
-          unsub();
-          reject(e);
-        }
-      } else {
-        html = null;
-      }
-    };
-    unsub = store.subscribe(() => {
-      dispatched = true;
-      setImmediate(checkSub);
-    });
-    try {
-      html = renderer({ store });
-    } catch (e) {
-      reject(e);
-      return;
-    }
-    if (timeout > 0) {
-      to = setTimeout(() => {
-        unsub();
-        reject(new Error(`Timeout of ${timeout}ms exceeded.`));
-      }, timeout);
-    }
-    setImmediate(checkSub);
+export default async function appServer(renderer, store, timeout = 30000, next = taut) {
+  let html;
+  let to;
+  let unsubDispatchWatch = null;
+  let dispatched = false;
+
+  if (timeout > 0) {
+    to = setTimeout(() => {
+      unsubDispatchWatch && unsubDispatchWatch();
+      throw new Error(`Timeout of ${timeout}ms exceeded.`);
+    }, timeout);
+  }
+
+  unsubDispatchWatch = store.subscribe(() => {
+    dispatched = true;
   });
+
+  const tryRender = async () => {
+    const finalState = store.getState();
+    const requests = getRequests(selectUniversal(finalState));
+    if (requests.length === 0 || !next(finalState)) {
+      try {
+        const nextHtml = dispatched ? renderer({ store }) : html;
+        if (dispatched) {
+          // if you dispatched during render will do one final render after requests have resolved
+          // giving you a chance to mount child containers
+          dispatched = false;
+          html = nextHtml;
+          return tryRender();
+        }
+        clearTimeout(to);
+        unsubDispatchWatch();
+        return { html: nextHtml, state: finalState };
+      } catch (e) {
+        unsubDispatchWatch();
+        throw e;
+      }
+    } else {
+      await new Promise((resolve) => {
+        const nextSub = store.subscribe(() => {
+          dispatched = true;
+          nextSub();
+          resolve();
+        });
+      });
+      return tryRender();
+    }
+  };
+  try {
+    html = renderer({ store });
+  } catch (e) {
+    throw e;
+  }
+  return tryRender();
 }
